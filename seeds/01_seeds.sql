15ce90871b5efa1a12ffebca2551cde32b6ba59b
Ina Ricci <ina_ricci@hotmail.com>
2024-03-11 02:13:01 -0700

    fixed order of the fields

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,11 +4,11 @@ VALUES
        ('Timothee Chalamet', 'timothee@dunecast.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
        ('Austin Butler', 'austin@dunecast.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
-INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, active)
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, street, city, province, post_code, country, cost_per_night,parking_spaces, number_of_bathrooms, number_of_bedrooms, active)
 VALUES 
-      (1, 'Dune', 'Description', 'image1.jpg', 'coverphoto1.jpg', 10, 'Dessert', 'Fremen', 'Arrakis', '12345', 'Arrakis', 0, 2, 2, TRUE),
-       (2, 'House Atreides', 'Description', 'image2.jpg', 'coverphoto2.jpg', 10000, 'Atreides', 'Castle', 'Arrakis', '45678', 'Arrakis', 100, 10, 10, TRUE),
-       (3, 'House Harkonnen', 'Description', 'thumbnail3.jpg', 'coverphoto3.jpg', 500, 'Castle', 'Harkonnen', 'Arrakis', '6789', 'Arrakis', 10, 5, 3, TRUE);
+      (1, 'Dune', 'Description', 'image1.jpg', 'coverphoto1.jpg',  'Dessert', 'Fremen', 'Arrakis', '12345', 'Arrakis', 10, 0, 2, 2, TRUE),
+       (2, 'House Atreides', 'Description', 'image2.jpg', 'coverphoto2.jpg',  'Atreides', 'Castle', 'Arrakis', '45678', 'Arrakis', 10000, 100, 10, 10, TRUE),
+       (3, 'House Harkonnen', 'Description', 'thumbnail3.jpg', 'coverphoto3.jpg', 'Castle', 'Harkonnen', 'Arrakis', '6789', 'Arrakis', 500,  10, 5, 3, TRUE);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 2, 3),
